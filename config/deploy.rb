106bcbd318b3cd37006ebe7a9f62ed106c6ea8dd
Michael Wimpee <m@emicus.com>
2009-11-14 09:59:04 -0800

    Changing deploy directory, setting up for deploy from git

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,14 @@ set :repository,  "git://github.com/jbr/breakglass.git"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/site/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
 
 role :app, "breakglass.emicus.com"
 role :web, "breakglass.emicus.com"
 role :db,  "breakglass.emicus.com", :primary => true
+
+default_run_options[:pty] = true
