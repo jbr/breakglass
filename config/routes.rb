ecc23c9464692468657cd403d642891b6876a46e
Jacob Rothstein <github@jacobrothstein.com>
2009-11-14 12:58:53 -0800

    cache w/ manifest

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :meeting_places
   map.resource :messages
   map.resource :twilio
+  
+  map.manifest '/manifest', :controller => 'manifests', :action => 'index'
   map.root :controller => 'families', :action => 'index'
 end
