c7a6ead6ca9eaaaabc9017488867eb01c60cfbe6
Cameron Ferroni <cameronf@cferroni.local>
2009-11-14 02:40:36 -0800

    added a save after the new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
 
   def create
+    # HACK HACK - need to actually make sure someone is logged in
+    session[:person_id] = 1
+    # end HACK HACK
     @message = Message.new(:text => params[:message][:text], :person_id => session[:person_id] )
+    @message.save
     render :text => nil
   end
 
