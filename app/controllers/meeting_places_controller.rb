9ac00e300d431b34ab5fe144e076c1430681aa5e
Jacob Rothstein <github@jacobrothstein.com>
2009-11-14 13:27:56 -0800

    meeting places (partially)

--- app/controllers/meeting_places_controller.rb
+++ app/controllers/meeting_places_controller.rb
@@ -1,25 +1,4 @@
 class MeetingPlacesController < ApplicationController
   before_filter :login_required
 
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
 end
