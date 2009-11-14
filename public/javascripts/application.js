46fc747a1da573d4137645d2d770bcf1bc4a68e3
Jacob Rothstein <github@jacobrothstein.com>
2009-11-14 14:02:38 -0800

    live event binding

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('.meeting-place').click(function(){
+  $('.meeting-place').live('click', function(){
     if($(this).find('.static').is(':visible')) {
       $(this).find('.static, .meeting-place-form').toggle('blind')
     }
