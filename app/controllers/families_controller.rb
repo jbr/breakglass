5b2ab56d4540dcf25ba56929dfbdfaffce52e229
Cameron Ferroni <cameronf@cferroni.local>
2009-11-14 00:49:34 -0800

    Actually added the files we needed

---
+++ app/controllers/families_controller.rb
@@ -0,0 +1,10 @@
+class FamiliesController < ApplicationController
+
+  def show
+    @family = Family.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.mobile
+    end
+  end
+end
