f584404a7a1efaf5c8170067d307744e63dd17f3
Jacob Rothstein <github@jacobrothstein.com>
2009-11-13 22:37:18 -0800

    person test and phone format stripping

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,15 @@
 class Person < ActiveRecord::Base
 	belongs_to :family
 	has_many :messages
+	validates_format_of :phone, :with => /[0-9]{10}/
 
 	validates_presence_of :family
+	
+	def self.strip_nonnumeric(phone)
+	  phone.gsub(/[^0-9]/, '')
+  end
+  
+  def phone=(phone)
+    self[:phone] = Person.strip_nonnumeric phone
+  end
 end
--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context 'with a new person' do
+    setup {@person = Person.new}
+    should 'not be valid' do
+      assert_not_valid @person
+      assert_not_nil @person.errors.on(:phone)
+    end
+    
+    context 'with an invalid phone' do
+      setup {@person.phone = '123456'}
+      should 'not be valid' do
+        assert_not_valid @person
+        assert_not_nil @person.errors.on(:phone)
+      end
+    end
+    
+    context 'phone format stripping' do
+      setup {@person.phone = "(123) 456 - 7890"}
+      should 'strip all nonnumeric characters on assignment' do
+        assert_phone '1234567890', @person
+      end
+    end
+    
+    context 'with valid attributes' do
+      setup do
+        @person.attributes = {
+          :phone => '(123) 456-7890',
+          :family => Family.new
+        }
+      end
+      
+      should 'be valid' do
+        assert_valid @person
+      end
+    end
   end
 end
