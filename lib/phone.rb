a936c5066b8e93523a267f2ec27192392ab01b50
Jacob Rothstein <github@jacobrothstein.com>
2009-11-14 10:34:14 -0800

    phone like fields

---
+++ lib/phone.rb
@@ -0,0 +1,20 @@
+class Phone
+  module Methods
+    def strip_nonnumeric(phone)
+  	  phone.gsub /[^0-9]/, ''
+    end
+  	
+    def phone_like_field(field_name)
+      define_method :"#{field_name}=" do |content|
+        self[field_name.to_s] = self.class.strip_nonnumeric content
+      end
+      
+      validates_format_of :phone, :with => /[0-9]{10}/
+      validates_uniqueness_of :phone
+    end
+    
+    def phone_like_fields(*field_names)
+      field_names.each {|p| phone_like_field p}
+    end
+  end
+end
