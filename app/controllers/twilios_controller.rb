b375b903c3eb09cdf576efffc7b3b331ed947e8a
Jeremy Johnstone <jstone@awaytable-lmcorpyahoocom.local>
2009-11-14 01:51:04 -0800

    Adding a basic hacked out method which generates TwiXML

--- app/controllers/twilios_controller.rb
+++ app/controllers/twilios_controller.rb
@@ -1,5 +1,7 @@
 class TwiliosController < ApplicationController
   def show
-
+    random_code = params[:pin].gsub(/\d/) {|s| s + ' '}
+    render :xml => Twilio::Verb.say("Your pin code is #{random_code}", 
+          :loop => 3, :pause => true)
   end
 end
