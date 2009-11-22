fba4ff0fb2e45632da8d42835205cd70291e8cf1
Jacob Rothstein <github@jacobrothstein.com>
2009-11-22 15:47:19 -0800

    test families controller

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,6 +1,7 @@
 class FamiliesController < ApplicationController
 	before_filter :login_required
-	load_resource :family, :except => :index
+	load_resource :family, :except => :index, :if_nil => :access_denied, :by => :id
+	before_filter :family_must_be_current_family, :except => :index
 	
 	def index
 	  @family = current_family
@@ -15,8 +16,10 @@ class FamiliesController < ApplicationController
   end
 
 	def update
+	  @success = @family.update_attributes params[:family]
+	  
 		respond_to do |format|
-			if @family.update_attributes(params[:family])
+			if @success
 				format.html { redirect_to(@family) }
 				format.xml  { head :ok }
 				format.json { head :ok }
@@ -27,4 +30,12 @@ class FamiliesController < ApplicationController
 			end
 		end
 	end
+	
+	private
+	def family_must_be_current_family
+  	unless @family == current_family
+      redirect_to family_url(current_family)
+      return false
+    end
+  end
 end
--- test/functional/families_controller_test.rb
+++ test/functional/families_controller_test.rb
@@ -9,15 +9,74 @@ class FamiliesControllerTest < ActionController::TestCase
       end
       
       should 'render the show action for the current family' do
-        assert_css ".family##{@person.family.element_id}"
+        assert_showing_family @person.family
       end
     end
     
     context 'when not logged in' do
-      setup {get :index}
+      setup { get :index }
       should 'redirect to log in url' do
         assert_redirected_to new_session_url
       end
     end
+  end
+  
+  context 'show' do
+    context 'when logged in' do
+      setup { @person = log_in_as :cameron }
+
+      context 'when viewing another family' do
+        setup { get :show, :id => families(:rothstein).to_param }
+      
+        should 'redirect to the current family' do
+          assert_redirected_to family_url(@person.family)
+        end
+      end
+      
+      context 'when viewing the correct family' do
+        setup { get :show, :id => @person.family.to_param }
+        
+        should 'show the family' do
+          assert_showing_family @person.family
+        end
+      end
+    end
+  end
+  
+  context 'update' do
+    context 'when logged in' do
+      setup { @person = log_in_as :cameron }
+      context 'when updating another family' do
+        setup do
+          post :update, :id => families(:rothstein).to_param, :family => { :name => 'mud' }
+        end
+        
+        should 'redirect to the current family (for now)' do
+          assert_redirected_to family_url(@person.family)
+        end
+        
+        should 'not update the family' do
+          assert_not_name 'mud', families(:rothstein)
+        end
+      end
+      
+      context 'when updating the correct family' do
+        setup do
+          post :update, :id => @person.family.to_param, :family => { :name => 'updated' }
+        end
+        
+        should 'update the family correctly' do
+          assert_name 'updated', @person.family.reload
+        end
+        
+        should 'redirect to show the family' do
+          assert_redirected_to family_url(@person.family)
+        end
+      end
+    end
+  end
+  
+  def assert_showing_family(family)
+    assert_css ".family##{family.element_id}"
   end
 end
