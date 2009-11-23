b9c261fcb238c4ffad35209a96589e7f3dd216bc
Jacob Rothstein <github@jacobrothstein.com>
2009-11-22 20:59:48 -0800

    touch up preinitializer to use system instead of backticks

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -2,6 +2,6 @@ begin
   require "#{File.dirname(__FILE__)}/../vendor/bundler_gems/environment"
 rescue LoadError
   puts "Bundling Gems\n\nHang in there, this only has to happen once...\n\n"
-  puts `gem bundle`
+  system 'gem bundle'
   retry
 end
