41b62be9c683462536502c71d59c3b73029d7a4c
Jacob Rothstein <github@jacobrothstein.com>
2009-11-22 20:54:59 -0800

    fancy preinitializer

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,2 +1,7 @@
-require "#{File.dirname(__FILE__)}/../vendor/bundler_gems/environment"
- 
+begin
+  require "#{File.dirname(__FILE__)}/../vendor/bundler_gems/environment"
+rescue LoadError
+  puts "Bundling Gems\n\nHang in there, this only has to happen once...\n\n"
+  puts `gem bundle`
+  retry
+end
