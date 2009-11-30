ebd7d15c726d0306a4340e6e4f614811630ad82d
Jacob Rothstein <github@jacobrothstein.com>
2009-11-30 15:00:14 -0800

    extract message broadcast from Message class, move it to Sms and Voice and provide a uniform api

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,51 +1,18 @@
+require 'set'
 class Message < ActiveRecord::Base
+  cattr_reader :handler_classes
 	belongs_to :person
-
-	validates_presence_of :person
-
+  validates_presence_of :person
 	after_create :broadcast
 
+  @@handler_classes = Set.new << Sms << Voice
+
 	private
-
-  def setup_outbound_conns
-    @sms = Clickatell::API.authenticate('3205392', 'breakglass', 'rhok2009')
-    Twilio.connect('AC22b634ba0e4e36e926222a3ae89a72ea', '82b27d73fa88a8dd599a12172e8c0bd3')
-    
-		@caller_id = "2063358014"
-    @twilio_cb_url = "http://breakglass.emicus.com/twilio?msg=#{CGI.escape(text)}" 
-  end
-
+	
 	def broadcast
-    setup_outbound_conns
-
-    self.person.family.people.each do |person|
-      if !person.sms.nil?
-        send_sms person.sms
-      end
-      
-      if !person.phone.nil?
-        voice_call person.phone
-      end
-      
-      if !person.email.nil?
-        send_email person.email
-      end
+	  Message.handler_classes.each do |handler_class|
+	    handler = handler_class.new self
+      person.family_members.each {|person| handler.contact person}
     end
-  end
-
-  def send_sms(cell)
-    puts "Sending sms: #{text} to: #{cell}"
-		# This is stupid, don't prepend the 1:
-    @sms.send_message("1#{cell}", text)
-  end
-
-  def voice_call(phone)
-    puts "Voice calling: #{phone} and telling them: #{text}"
-    Twilio::Call.make(@caller_id, phone, @twilio_cb_url)
-  end
-
-  def send_email(address)
-    puts "Sending email: #{text} to: #{address}"
-    #TODO: send emails!
   end
 end
--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context 'message handler classes' do
+    should 'register two handlers (hardcoded for now)' do
+      assert_equal Set.new([Sms, Voice]), Message.handler_classes
+    end
+  end
+  
+  context 'creating a new message' do
+    setup {@message = Message.new :person => people(:cameron), :text => 'testing'}
+    teardown {@message.save!}
+    
+    should 'create exactly one handler for each registered handler class' do
+      Message.handler_classes.each do |handler_class|
+        flexmock(handler_class).should_receive(:new).once.and_return flexmock(:contact => nil)
+      end
+    end
+    
+    should 'call contact once for each family member' do
+      Message.handler_classes.each do |handler_class|
+        flexmock(handler_class).new_instances.should_receive(:contact).times @message.person.family_members.size
+      end
+    end
   end
 end
