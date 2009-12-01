4cf0520bc6d6cd4f6f6a8c15ebf5eca9f95d9eac
Jacob Rothstein <github@jacobrothstein.com>
2009-11-30 16:52:28 -0800

    remove xml functionality, since it's currently not in spec

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -21,11 +21,9 @@ class FamiliesController < ApplicationController
 		respond_to do |format|
 			if @success
 				format.html { redirect_to(@family) }
-				format.xml  { head :ok }
 				format.json { head :ok }
 			else
 				format.html { render :action => "edit" }
-				format.xml  { render :xml  => @record.errors.to_xml,  :status => :unprocessable_entity }
 				format.json { render :json => @record.errors.to_json, :status => :unprocessable_entity }
 			end
 		end
