630934fd6de574123fada5aeb4ba05855e270eae
Jacob Rothstein <github@jacobrothstein.com>
2010-01-10 17:14:23 -0800

    convert to haml

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ bundle_path "vendor/bundler_gems"
 gem "twilio", '2.5.0'
 gem "clickatell", '0.7.1'
 gem 'rails', '2.3.5'
+gem 'haml', '2.2.14'
 
 only :test do
   gem "test-rig", '0.0.2', :require_as => "test_rig"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def default_value_text_field(f, field_name, default_value)
+    f.text_field field_name, :size => 16, :value => default_value,
+      :onFocus => "if(this.value==this.defaultValue) this.value='';",
+      :onBlur => "if(this.value=='') this.value=this.defaultValue;"
+  end
 end
